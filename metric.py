2ac30a99b50a19a0a9a3c74435f78205a0443c99
sssrabit <60918615+sssrabit@users.noreply.github.com>
2020-10-27 14:28:53 +0800

    Add files via upload

--- metric.py
+++ metric.py
@@ -22,9 +22,8 @@ def iou(predict_bb,gt_bb):
 
 def AP(bb_confidence,bb_iou,gt_number,threshold = 0.5):
 	fp,tp,ap = 0,0,0
+	# sort iou
 	x,y = [list(h) for h in zip(*sorted(zip(bb_confidence,bb_iou),reverse = True))] #x : sorted confidence, y : sorted iou
-	print(x)
-	print(y)
 	for i in range(len(x)):
 		if y[i] < threshold:
 			fp += 1
@@ -42,6 +41,7 @@ def AP(bb_confidence,bb_iou,gt_number,threshold = 0.5):
 def compute(clas,ground_truth,predict,threshold = 0.3):
 	fn = 0
 	for img in range(len(ground_truth)):
+		# find class
 		gt_bb_list = []
 		for i in range(len(ground_truth[img])):
 			if ground_truth[img][i][0] == clas:
@@ -50,8 +50,14 @@ def compute(clas,ground_truth,predict,threshold = 0.3):
 		for i in range(len(predict[img])):
 			if predict[img][i][0] == clas:
 				pr_bb_list.append(predict[img][i])
-		if pr_bb_list == []:
-			return 0,0,0,0
+		
+		if pr_bb_list == [] and gt_bb_list == []:
+			return 0,0,0,0,0
+		elif pr_bb_list and gt_bb_list == []:
+			return 0,len(pr_bb_list),0,0,1
+		elif pr_bb_list == [] and gt_bb_list:
+			return len(gt_bb_list),0,0,0,1
+
 		iou_table = [[-1 for i in range(len(pr_bb_list))] for j in range(len(gt_bb_list))]
 		for i in range(len(gt_bb_list)):
 			gt_bb = [float(gt_bb_list[i][1])-float(gt_bb_list[i][3])/2,float(gt_bb_list[i][2])-float(gt_bb_list[i][4])/2,float(gt_bb_list[i][1])+float(gt_bb_list[i][3])/2,float(gt_bb_list[i][2])+float(gt_bb_list[i][4])/2]
@@ -72,7 +78,7 @@ def compute(clas,ground_truth,predict,threshold = 0.3):
 
 		fp,tp,ap = AP(bb_confidence,bb_iou,len(gt_bb_list))
 						#ap = AP(iou_table,pr_bb_list)
-	return fn,fp,tp,ap
+	return fn,fp,tp,ap,1
 
 ## read file gt : [img][bb number][class,x,y,w,h] pr : [img][bb number][class,confidence,x,y,w,h]
 path = os.getcwd()
@@ -88,17 +94,19 @@ for pr in predict_list:
 FN,FP,TP,SAP = 0,0,0,0
 category = 0
 for i in range(36):
-	fn,fp,tp,ap = compute(str(i),ground_truth,predict)
+	fn,fp,tp,ap,flag = compute(str(i),ground_truth,predict)
 	TP += tp
 	FN += fn
 	FP += fp
 	SAP += ap
-	if fn == 0 and fp == 0 and tp == 0 and ap == 0:
-		continue
-	else:
+	if flag == 1:
 		category += 1
+
 print("TP : ",TP)
 print("FP : ",FP)
 print("FN : ",FN)
-print("Accuracy : ",TP/(FN+FP+TP))
+if FN+FP+TP == 0:
+	print("Accuracy : ",0)
+else:
+	print("Accuracy : ",TP/(FN+FP+TP))
 print("mAP : ",SAP/category)
